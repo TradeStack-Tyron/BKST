b41ea1a9391b4968107d6166bde77bbc02a36bb8
Tyron <tyronodame5@gmail.com>
2025-07-16 20:15:28 -0400

    created a landing page and login page

---
+++ bskt_app/frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { useState } from 'react';
+
+import '../Login.css';
+
+const LoginPage = () => {
+  // setting useStates for the form
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className="bg-black">
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        >
+          <div
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+            className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
+          />
+        </div>
+
+        <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:py-32">
+          <div className="login-outer-div">
+            <form action="">
+              <h1 className="text-5xl font-semibold tracking-tight text-balance text-purple-200 sm:text-7xl mb-8">
+                BKST.
+              </h1>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button type="submit">Login</button>
+              <div className="w-full border-t border-purple-300/30 my-6"></div>
+              <div className="flex items-center justify-center gap-4 whitespace-nowrap">
+                <span className="text-purple-200 text-xs">
+                  If you don't have an account
+                </span>
+                <a
+                  href="/signup"
+                  className="text-purple-300 hover:text-purple-200 font-medium text-[12px]"
+                >
+                  Sign up
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div
+          aria-hidden="true"
+          className="bg-black-200 absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        >
+          <div
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+            className="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
